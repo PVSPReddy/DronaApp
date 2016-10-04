5792093a349f3c63cec9e499c00adfa49f5dbae9
Rabbit <venkatasivaprasadreddypulagam@gmail.com>
2016-10-04 18:38:05 +0530

    committing android pages and display day pages in download project

---
+++ DronaApp/Droid/Services/IEmailService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace DronaApp.Droid
+{
+	public class IEmailService : ContentPage
+	{
+		public IEmailService()
+		{
+			Content = new StackLayout
+			{
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
+
