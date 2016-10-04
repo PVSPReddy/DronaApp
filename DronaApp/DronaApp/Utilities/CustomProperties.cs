759487119a9bb1dc0658b8182d317d69b9b46b15
DESKTOP-OS2NOSH\Sivaprasad <venkatasivaprasadreddypulagam@gmail.com>
2016-10-04 22:23:24 +0530

    committing custom properties

--- DronaApp/DronaApp/Utilities/CustomProperties.cs
+++ DronaApp/DronaApp/Utilities/CustomProperties.cs
@@ -6,14 +6,44 @@ namespace DronaApp
 {
 	public class CustomProperties : ContentPage
 	{
+       private double appScreenHeight;
+		private double appScreenWidth;
+		private double appHeadderHeight;
+		private double appHeaderWidth;
+
+
+		public dynamic AppScreenHeight
+		{
+			get { return appScreenHeight; }
+			set { appScreenHeight = value; }
+		}
+
+		public dynamic AppScreenWidth
+		{
+			get { return appScreenWidth; }
+			set { appScreenWidth = value; }
+		}
+		public dynamic AppHeadderHeight
+		{
+			get { return appHeadderHeight; }
+			set { appHeadderHeight = value; }
+		}
+		public dynamic AppHeaderWidth
+		{
+			get { return appHeaderWidth; }
+			set { appHeaderWidth = value; }
+		}
+
+		/*#region
+		 #endregion*/
+
+
 		public CustomProperties()
 		{
-			Content = new StackLayout
-			{
-				Children = {
-					new Label { Text = "Hello ContentPage" }
-				}
-			};
+			appScreenHeight = App.ScreenHeight + 3;
+			appScreenWidth = App.ScreenWidth + 3;
+			appHeadderHeight = (appScreenHeight * 10) / 100;
+			appHeaderWidth = appScreenWidth;
 		}
 	}
 }
