fd77281ce9f3e76849935f2e6fe89dad0b5b27b8
Rabbit <venkatasivaprasadreddypulagam@gmail.com>
2016-10-07 10:10:44 +0530

    camera project initiation files in iOS and droid

---
+++ DronaApp/Droid/Services/ICameraGalleryService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace DronaApp.Droid
+{
+	public class ICameraGalleryService : ContentPage
+	{
+		public ICameraGalleryService()
+		{
+			Content = new StackLayout
+			{
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
+
