185f6e3cc0c9f37e960ac6f9ceebb08956dbe89d
Rabbit <venkatasivaprasadreddypulagam@gmail.com>
2016-10-12 17:42:52 +0530

    initialising new style of accessing activity in tamarin.forms by crating new activity in the dependency so that main activity can be left free with less lines of code single dependency with onactivtyresult handler and also the dependency which activates it

---
+++ DronaApp/Droid/Services/ICameraGalleryServiceTwo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace DronaApp.Droid
+{
+	public class ICameraGalleryServiceTwo : ContentPage
+	{
+		public ICameraGalleryServiceTwo()
+		{
+			Content = new StackLayout
+			{
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
+
