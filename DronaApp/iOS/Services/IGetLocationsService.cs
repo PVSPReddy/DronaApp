3b0ce8f2cc421b7322310aeee2fb964d28292acb
Rabbit <venkatasivaprasadreddypulagam@gmail.com>
2017-01-25 19:01:19 +0530

    drona map coordinates

---
+++ DronaApp/iOS/Services/IGetLocationsService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace DronaApp.iOS
+{
+	public class IGetLocationsService : ContentPage
+	{
+		public IGetLocationsService()
+		{
+			Content = new StackLayout
+			{
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
